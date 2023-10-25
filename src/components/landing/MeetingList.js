8def5aaf88f9eab0919bd9f898d7c4ba4b7376d5
Beastoloch <maks.serebrov.94@gmail.com>
2023-10-25 16:03:16 +0700

    feat/сверстать авторизацию

---
+++ src/components/landing/MeetingList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MeetingList() {
+
+
+
+    return(
+        <h1>тут листы короче</h1>
+    );
+}
+
+export default MeetingList
---
+++ src/components/landing/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/NavBar.css'
+import {NavLink} from "react-router-dom";
+
+function NavBar() {
+
+
+
+    return(
+        <nav className='menu'>
+            <NavLink to='/meeting-list' className={({isActive}) => `menu__link 
+                ${isActive ? "menu__link_active" : ""}`}>Домой</NavLink>
+            <NavLink to='/profile' className={({isActive}) => `menu__link 
+                ${isActive ? "menu__link_active" : ""}`}>Личный кабинет</NavLink>
+        </nav>
+    );
+}
+
+export default NavBar
